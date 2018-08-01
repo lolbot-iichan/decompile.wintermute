d7507a2471035ce46e85bd9318f958ce9fdb2459
lolbot-iichan <lolbot_iichan@mail.ru>
2018-08-02 02:40:03 +0300

    added GPLv2, fixed a few misprints

--- decompile_wintermute.py
+++ decompile_wintermute.py
@@ -1,6 +1,25 @@
-﻿# WinterMute Engine scripts decompiler v1.0
+﻿"""
+ " WinterMute Engine scripts decompiler v1.0
+ "
+ " This program is free software; you can redistribute it and/or
+ " modify it under the terms of the GNU General Public License
+ " as published by the Free Software Foundation; either version 2
+ " of the License, or (at your option) any later version.
+ "
+ " This program is distributed in the hope that it will be useful,
+ " but WITHOUT ANY WARRANTY; without even the implied warranty of
+ " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ " GNU General Public License for more details.
+ "
+ " You should have received a copy of the GNU General Public License
+ " along with this program; if not, write to the Free Software
+ " Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ "
+"""
+
+# WinterMute Engine scripts decompiler v1.0
 # I wrote this code while inspecting sources of "FoxTail" by Gingertips Studio for improving support of FoxTail at ScummVM. 
-# Pilepine is line this:
+# Pipeline is like this:
 # 1. wmeasm:       transform binary format to asm-like format
 # 2. wmemedium:    evaluate stack operations to transform non-branching code to "actor.GoTo(this.WalkToX, this.WalkToY);"
 # 3. wmehigh:      use some rules of thumb to detect while/switch/break/else constructions
@@ -16,7 +35,7 @@
 # ========
 # CONTACTS
 # ========
-# Copyleft by lolbot, member of IIchan.ru eroge project (who cares in 2018, lol).
+# Made by lolbot, member of IIchan.ru eroge project (who cares in 2018, lol).
 #         _                             
 #       ,' ".   ,-"-.                        
 #      :     '.'▄██▄ '.
